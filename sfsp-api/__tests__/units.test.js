3bd3b0831d6d969205a8dfb61da4348cd850a4a4
PLM-18 <philemonmuleya@gmail.com>
2025-06-07 16:54:01 +0200

    Add global definitions for Jest in integration and unit test files

--- sfsp-api/__tests__/units.test.js
+++ sfsp-api/__tests__/units.test.js
@@ -1,3 +1,4 @@
+/* global describe, it, expect, beforeEach, afterEach, jest */
 const UserController = require('../controllers/userController');
 const userService = require('../services/userService');
 
