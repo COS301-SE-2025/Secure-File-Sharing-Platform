ae59795c529373769cd8d053e319a6670927c94d
Tiyiso Wolfiez <tiyisoh6@gmail.com>
2025-06-12 11:39:59 +0200

    contact Services

--- sfsp-api/routes/index.js
+++ sfsp-api/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
 const fileRoutes = require('./fileRoutes');
+const contactRoutes = require('./contactRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/files', fileRoutes);
+router.use('/contact', contactRoutes);
 
 router.get('/health', (req, res) => {
     res.status(200).json({
