54cd64a76448d3fe3551197c55c9c55716e2bcf4
PLM-18 <philemonmuleya@gmail.com>
2025-06-25 10:27:19 +0200

    chaning methods

--- sfsp-api/routes/notificationRoutes.js
+++ sfsp-api/routes/notificationRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notificationController');
 
-router.post('/get', notificationController.getNotifications);
+router.get('/get', notificationController.getNotifications);
 router.post('/markAsRead', notificationController.markAsRead);
 router.post('/respond', notificationController.respondToShareRequest);
-router.post('/clear', notificationController.clearNotification);
+router.delete('/clear', notificationController.clearNotification);
 router.post('/add', notificationController.addNotification);
 
 module.exports = router;
