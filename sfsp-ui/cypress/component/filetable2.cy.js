cef173bb8cc84b678bb763f479f1917cb9e45813
Tiyiso Wolfiez <tiyisoh6@gmail.com>
2025-06-26 15:02:20 +0200

    patches

--- sfsp-ui/cypress/component/filetable2.cy.js
+++ sfsp-ui/cypress/component/filetable2.cy.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { mount } from 'cypress/react';
 import FileTable from '../../app/dashboard/components/FileTable';
 
 describe('FileTable', () => {
   it('renders table headers correctly', () => {
-    cy.mount(<FileTable files={[]} />);
+    mount(<FileTable files={[]} />);
     cy.get('table thead tr').within(() => {
       ['Name', 'Size', 'Shared by', 'Shared', 'Actions'].forEach((header) => {
         cy.contains('th', header).should('exist');
       });
-      cy.contains('th', 'Actions').should('have.class', 'text-right');
     });
   });
 
   it('renders empty table when no files are provided', () => {
-    cy.mount(<FileTable files={[]} />);
+    mount(<FileTable files={[]} />);
     cy.get('table tbody tr').should('not.exist');
   });
 });
