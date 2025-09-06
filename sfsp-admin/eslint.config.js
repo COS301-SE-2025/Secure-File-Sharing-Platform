14e50905aabc89741f35bf8171a8913d4e257461
simphiwe-jessica-zitha <u20463104@tuks.co.za>
2025-09-06 22:42:03 +0200

    fx to dev mode live updates in sfsp-admin

--- sfsp-admin/eslint.config.js
+++ sfsp-admin/eslint.config.js
@@ -6,6 +6,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -24,6 +25,14 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+
+  {
+    files: ['src/electron/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node, 
     },
   },
 ])
