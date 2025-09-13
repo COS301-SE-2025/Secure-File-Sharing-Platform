23c5382fab82588d83ef7f114b9c082a41f78297
simphiwe-jessica-zitha <u20463104@tuks.co.za>
2025-09-13 15:42:59 +0200

    Asset path fix

--- sfsp-admin/src/electron/pathResolver.js
+++ sfsp-admin/src/electron/pathResolver.js
@@ -6,13 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function getPreloadPath() {
-  return path.join(__dirname,'preload.cjs');
+  return path.join(__dirname, 'preload.cjs');
 }
 
-export function getUIPath(){
-  return path.join(app.getAppPath()+"/dist-react/index.html");
+export function getUIPath() {
+  return path.join(app.getAppPath() + "/dist-react/index.html");
 }
 
 export function getAssetPath() {
-  return path.join(app.getAppPath()+ '/src'+ '/assets');
+  if (app.isPackaged) {
+    return path.join(process.resourcesPath, "src", "assets");
+  } else {
+    return path.join(__dirname, "..", "assets");
+  }
 }
