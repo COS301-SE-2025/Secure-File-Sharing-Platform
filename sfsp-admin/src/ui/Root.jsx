1eb0d576565d6aa61e7ea2ef5bf64c902152234f
simphiwe-jessica-zitha <u20463104@tuks.co.za>
2025-09-20 17:43:51 +0200

    users page

--- sfsp-admin/src/ui/Root.jsx
+++ sfsp-admin/src/ui/Root.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Dashboard from "./dashboard/Dashboard.jsx";
+import Users from "./users/Users.jsx";
 import Layout from "./Layout.jsx";
 
 export default function Root() {
@@ -10,6 +11,7 @@ export default function Root() {
 
       <Route path="/" element={<Layout />}>
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
       </Route>
     </Routes>
   );
