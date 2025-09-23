59b6ff284ccd6bb1751bc59e253a330f1e3f5d62
simphiwe-jessica-zitha <u20463104@tuks.co.za>
2025-09-23 11:38:35 +0200

    fixes for dist:win by inclusion of hash routes

--- sfsp-admin/src/ui/Root.jsx
+++ sfsp-admin/src/ui/Root.jsx
@@ -1,18 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import Dashboard from "./dashboard/Dashboard.jsx";
 import Users from "./users/Users.jsx";
 import Layout from "./Layout.jsx";
 
+const Router = window.location.protocol === "file:" ? HashRouter : BrowserRouter;
+
 export default function Root() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-
-      <Route path="/" element={<Layout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="users" element={<Users />} />
-      </Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
+
+
+
+// import { Routes, Route } from "react-router-dom";
+// import App from "./App.jsx";
+// import Dashboard from "./dashboard/Dashboard.jsx";
+// import Users from "./users/Users.jsx";
+// import Layout from "./Layout.jsx";
+
+// export default function Root() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<App />} />
+
+//       <Route path="/" element={<Layout />}>
+//         <Route path="dashboard" element={<Dashboard />} />
+//         <Route path="users" element={<Users />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
--- sfsp-admin/src/ui/main.jsx
+++ sfsp-admin/src/ui/main.jsx
@@ -1,12 +1,24 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import Root from "./Root.jsx";
 
 createRoot(document.getElementById("root")).render(
-
-  <BrowserRouter>
+  <React.StrictMode>
     <Root />
-  </BrowserRouter>
-
+  </React.StrictMode>
 );
+
+
+
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import Root from "./Root.jsx";
+
+// createRoot(document.getElementById("root")).render(
+
+//   <BrowserRouter>
+//     <Root />
+//   </BrowserRouter>
+
+// );
